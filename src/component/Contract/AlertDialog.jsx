8fcc42e95827fb7b766dd7334368ce1be252ba79
Chiou Jia Sheng <100426464+Chious@users.noreply.github.com>
2023-10-05 10:47:10 +0800

    update

--- src/component/Contract/AlertDialog.jsx
+++ src/component/Contract/AlertDialog.jsx
@@ -1,48 +1,56 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
-export default function AlertDialog() {
-    const [open, setOpen] = React.useState(false);
+export default function AlertDialog({ open, setOpen }) {
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-    return (
-        <div>
-            <Button variant="outlined" onClick={handleClickOpen}>
-                Open alert dialog
-            </Button>
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">
-                    {"Thanks for your submisson"}
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        We would contract you soon!
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Disagree</Button>
-                    <Button onClick={handleClose} autoFocus>
-                        Agree
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </div>
-    );
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          display: "flex",
+        }}
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Thanks for your submisson"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText
+            id="alert-dialog-description"
+            sx={{ textAlign: "center" }}
+          >
+            We would contract you soon!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions sx={{ justifyContent: "center", display: "flex" }}>
+          <Button
+            onClick={handleClose}
+            autoFocus
+            sx={{
+              width: 150,
+              borderBottom: 5,
+              borderRadius: 0,
+              borderColor: "#4EE1A0",
+              color: "rgb(36 36 36)",
+            }}
+          >
+            All right!
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
--- src/component/Project/Project.jsx
+++ src/component/Project/Project.jsx
@@ -1,35 +1,20 @@
-import { Card, Button, Grid, Typography, Box } from "@mui/material"
-import Image from 'mui-image'
-import { useState } from "react";
-
-
-import project from "./project.json"
-
-import logo1 from "../../assets/images/thumbnail-project-1-large.webp";
-import logo2 from "../../assets/images/thumbnail-project-2-large.webp";
-import logo3 from "../../assets/images/thumbnail-project-3-large.webp";
-import logo4 from "../../assets/images/thumbnail-project-4-large.webp";
-import logo5 from "../../assets/images/thumbnail-project-5-large.webp";
-import logo6 from "../../assets/images/thumbnail-project-6-large.webp";
+import { Grid, Box } from "@mui/material";
+import project from "./project.json";
 import ProjectCards from "./ProjectCards";
 
-
 export default function Project() {
+  var projects = project.map((e, i) => {
+    return <ProjectCards data={e} key={i} />;
+  });
 
-    var projects = project.map((e, i) => {
-        return (
-            <ProjectCards data={e} />
-        )
-    })
-
-
-
-    return (<>
-        <Box sx={{ marginLeft: "120px", marginRight: "120px" }}>
-            <h1>Projects</h1>
-            <Grid container spacing={1}>
-                {projects}
-            </Grid>
-        </Box>
-    </>)
+  return (
+    <>
+      <Box sx={{ marginLeft: "120px", marginRight: "120px" }}>
+        <h1>Projects</h1>
+        <Grid container spacing={1}>
+          {projects}
+        </Grid>
+      </Box>
+    </>
+  );
 }
